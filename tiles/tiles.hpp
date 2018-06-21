db6e679129e5a93581748f477482e5f551dde7c0
Shunji Lin <lgxshunji@gmail.com>
2018-06-22 00:19:52 +0900

    change array to vec for dynamic allocation

--- tiles/tiles.hpp
+++ tiles/tiles.hpp
@@ -1,7 +1,7 @@
 #ifndef TILES_HPP
 #define TILES_HPP
 
-#include <array>
+#include <vector>
 #include <initializer_list>
 #include <sstream>
 #include <iostream>
@@ -13,21 +13,16 @@ namespace Tiles {
     constexpr int WIDTH = 5, HEIGHT = 5, N_TILES = WIDTH * HEIGHT;
 
     class Node {
-        std::array<char, N_TILES> tiles;
-        char blank_idx = 0;
+        std::vector<char> tiles;
+        //char blank_idx = 0;
     public:
-        Node(std::initializer_list<char> tiles_list)  {
-            std::copy(tiles_list.begin(), tiles_list.end(), tiles.begin());
-            // temporary for testing
-            std::cout << *this;
-            std::cout << blank_idx;
-        }
+        Node(std::initializer_list<char> tiles_list) :
+            tiles(tiles_list) {}
         
         friend std::ostream &operator<<(std::ostream& os, const Node& node) {
             for (auto tile : node.tiles) {
                 os << std::to_string(tile) << " ";
             }
-            os << "\n";
             return os;
         }
     };
