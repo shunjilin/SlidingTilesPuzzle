138226137b13dfbd40ce1794eb76267d8064e2c9
Shunji Lin <lgxshunji@gmail.com>
2018-06-22 00:19:27 +0900

    change array to vec for dynamic allocation

--- test/tilesUnitTest.cpp
+++ test/tilesUnitTest.cpp
@@ -8,7 +8,7 @@ TEST(Tiles, InitializeTilesCorrect) {
                 16, 17, 18, 19, 20, 21, 22, 23, 24, 0});
     std::ostringstream oss;
     oss << node;
-    ASSERT_EQ(oss.str(), "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 0 \n");
+    ASSERT_EQ(oss.str(), "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 0 ");
 }
 
 int main(int argc, char *argv[]) {
