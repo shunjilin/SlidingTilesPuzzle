1dcea93a0bda63da42109da4d4be17ae8b581dfd
Shunji Lin <lgxshunji@gmail.com>
2018-06-25 17:56:18 +0900

    operations to manipulate board

--- test/tilesUnitTest.cpp
+++ test/tilesUnitTest.cpp
@@ -1,14 +1,37 @@
 #include "tiles.hpp"
 #include <string>
 #include <sstream>
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-TEST(Tiles, InitializeTilesCorrect) {
-    auto node = Tiles::Node({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ,14, 15,
-                16, 17, 18, 19, 20, 21, 22, 23, 24, 0});
+using namespace Tiles;
+
+class BoardInitialize: public testing::Test {
+public:
+    Board board = Board({1, 2, 3, 4, 0, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+                      15, 16, 17, 18, 19, 20, 21, 22, 23, 24});
+    /* 1  2  3  4  0
+       5  6  7  8  9
+       10 11 12 13 14
+       15 16 17 18 19
+       20 21 22 23 24 */
+};
+
+TEST_F(BoardInitialize, InitializeTilesCorrect) {
     std::ostringstream oss;
-    oss << node;
-    ASSERT_EQ(oss.str(), "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 0 ");
+    oss << board;
+    ASSERT_EQ(oss.str(), "1 2 3 4 0 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 ");
+}
+
+TEST_F(BoardInitialize, getCorretBlankPosition) {
+    ASSERT_EQ(getBlankIdx(board), 4);
+}
+
+TEST_F(BoardInitialize, getCorrectNewBlankTileIndexes) {
+    auto new_blank_idxs = getNewBlankIdxs(board);
+    EXPECT_EQ(new_blank_idxs.size(), 2);
+    EXPECT_THAT(new_blank_idxs, testing::Contains(3));
+    EXPECT_THAT(new_blank_idxs, testing::Contains(9));
 }
 
 int main(int argc, char *argv[]) {
--- tiles/tiles.hpp
+++ tiles/tiles.hpp
@@ -6,26 +6,66 @@
 #include <sstream>
 #include <iostream>
 #include <algorithm>
+#include <array>
 #include <string>
 
 namespace Tiles {
 
     constexpr int WIDTH = 5, HEIGHT = 5, N_TILES = WIDTH * HEIGHT;
 
-    class Node {
+    struct Board {
         std::vector<char> tiles;
-        //char blank_idx = 0;
-    public:
-        Node(std::initializer_list<char> tiles_list) :
+
+        Board(std::initializer_list<char> tiles_list) :
             tiles(tiles_list) {}
         
-        friend std::ostream &operator<<(std::ostream& os, const Node& node) {
-            for (auto tile : node.tiles) {
+        friend std::ostream &operator<<(std::ostream& os, const Board& board) {
+            for (auto tile : board.tiles) {
                 os << std::to_string(tile) << " ";
             }
             return os;
         }
     };
+
+    // free functions to manipulate the board
+    
+    // TODO: consider just caching this
+    // get index of current blank tile
+    char getBlankIdx(const Board& board) {
+        auto blank_iter = std::find(board.tiles.begin(), board.tiles.end(), 0);
+        return std::distance(board.tiles.begin(), blank_iter);
+    }
+
+    // direction that the blank tile moves to get new blank tile position
+    enum class DIRECTION { UP, DOWN, LEFT, RIGHT };
+
+    // TODO: consider reordering / shuffling
+    const std::array<DIRECTION, 4> directions =
+    { {DIRECTION::UP, DIRECTION::DOWN, DIRECTION::LEFT, DIRECTION::RIGHT} };
+
+    // get possible new blank indexes
+    std::vector<char> getNewBlankIdxs(const Board& board) {
+        auto blank_idx = getBlankIdx(board);
+        std::vector<char> new_blank_idxs;
+        // only accept and push valid indexes
+        for (auto direction : directions) {
+            if (direction == DIRECTION::UP && blank_idx >= WIDTH) {
+                new_blank_idxs.push_back(blank_idx - WIDTH);
+            }
+            if (direction == DIRECTION::DOWN &&
+                blank_idx < (WIDTH * (HEIGHT - 1))) {
+                new_blank_idxs.push_back(blank_idx + WIDTH);
+            }
+            if (direction == DIRECTION::LEFT && (blank_idx % WIDTH) != 0) {
+                new_blank_idxs.push_back(blank_idx - 1);
+            }
+            if (direction == DIRECTION::RIGHT &&
+                (blank_idx % WIDTH) != (WIDTH - 1)) {
+                new_blank_idxs.push_back(blank_idx + 1);
+            }
+        }
+        return new_blank_idxs;
+    }
 
 }
 
