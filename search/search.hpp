8cc12af0189a9f3b43105a0f8a24878f59aebc5f
Shunji Lin <lgxshunji@gmail.com>
2018-06-27 21:06:31 +0900

    add abstract virtual search base class

--- search/search.hpp
+++ search/search.hpp
@@ -1,6 +1,16 @@
 #ifndef SEARCH_HPP
 #define SEARCH_HPP
 
+#include "tiles.hpp"
+#include "heuristics.hpp"
+#include <memory>
 
+struct Search {
+
+    virtual std::vector<Tiles::Board>
+    search(Tiles::Board const& initial_board,
+           std::unique_ptr<Tiles::Heuristic> heuristic) = 0;
+    
+};
 
 #endif
