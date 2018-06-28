f1b3504022bb33469f9a1c7806d79cbefea18f94
Shunji Lin <lgxshunji@gmail.com>
2018-06-28 20:11:42 +0900

    add TODO: remove reverse operator

--- tiles/tiles.cpp
+++ tiles/tiles.cpp
@@ -37,6 +37,7 @@ namespace Tiles {
 
     // get possible new blank indexes
     // TODO: hardcode and optimize this to reduce branching?
+    // TODO: eliminate operation that gives parent state
     std::array<char, static_cast<int>(DIRECTION::COUNT)>
     getNewBlankIdxs(const Board& board) {
         auto blank_idx = board.cur_blank_idx == -1 ?
