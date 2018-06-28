f50c63307b82f8500d064575f7745a2246a7db85
Shunji Lin <lgxshunji@gmail.com>
2018-06-28 20:54:10 +0900

    refactor tile generation

--- tiles/tiles.cpp
+++ tiles/tiles.cpp
@@ -11,9 +11,11 @@ namespace Tiles {
           15, 16, 17, 18, 19,
           20, 21, 22, 23, 24});
 
+    // TODO: set blank index?
     Board::Board(const std::array<char, N_TILES>& tiles) :
         tiles(tiles) {}
-    
+
+    // TODO: pretty print
     std::ostream &operator<<(std::ostream& os, const Board& board) {
         for (auto tile : board.tiles) {
             os << std::to_string(tile) << " ";
@@ -22,11 +24,6 @@ namespace Tiles {
     }
 
     // free functions to manipulate the board
-
-    // TODO: consider reordering / shuffling
-    constexpr std::array<DIRECTION, static_cast<int>(DIRECTION::COUNT)>
-    directions =
-    { {DIRECTION::UP, DIRECTION::DOWN, DIRECTION::LEFT, DIRECTION::RIGHT} };
     
     // TODO: consider just caching this
     // get index of current blank tile
@@ -44,25 +41,22 @@ namespace Tiles {
             getBlankIdx(board) : board.cur_blank_idx;
 
         // -1 for invalid move
-        std::array<char, static_cast<int>(DIRECTION::COUNT)>
+        std::array<char, DIRECTION::COUNT>
             new_blank_idxs = {-1, -1, -1, -1};
-        
-        for (std::size_t i = 0; i < directions.size(); ++i) {
-            if (directions[i] == DIRECTION::UP && blank_idx >= WIDTH) {
-                new_blank_idxs[i] = blank_idx - WIDTH;
-            }
-            if (directions[i] == DIRECTION::DOWN &&
-                blank_idx < (WIDTH * (HEIGHT - 1))) {
-                new_blank_idxs[i] = blank_idx + WIDTH;
-            }
-            if (directions[i] == DIRECTION::LEFT && (blank_idx % WIDTH) != 0) {
-                new_blank_idxs[i] = blank_idx - 1;
-            }
-            if (directions[i] == DIRECTION::RIGHT &&
-                (blank_idx % WIDTH) != (WIDTH - 1)) {
-                new_blank_idxs[i] = blank_idx + 1;
-            }
+
+        if (blank_idx >= WIDTH) {
+            new_blank_idxs[DIRECTION::UP] = blank_idx - WIDTH;
         }
+        if (blank_idx < (WIDTH * (HEIGHT - 1))) {
+            new_blank_idxs[DIRECTION::DOWN] = blank_idx + WIDTH;
+        }
+        if ((blank_idx % WIDTH) != 0) {
+            new_blank_idxs[DIRECTION::LEFT] = blank_idx - 1;
+        }
+        if ((blank_idx % WIDTH) != (WIDTH - 1)) {
+            new_blank_idxs[DIRECTION::RIGHT] = blank_idx + 1;
+        }
+        
         return new_blank_idxs;
     }
 
