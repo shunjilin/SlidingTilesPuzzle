e87d589c573006b4002b4ce2f324fb0a599f357f
Shunji Lin <lgxshunji@gmail.com>
2018-06-28 20:54:10 +0900

    refactor tile generation

--- tiles/tiles.hpp
+++ tiles/tiles.hpp
@@ -14,9 +14,14 @@ namespace Tiles {
     constexpr int WIDTH = 5, HEIGHT = 5, N_TILES = WIDTH * HEIGHT;
 
     struct Board {
-        // TODO: consider using more compact representation for this,
-        // e.g. through a custom class that supports indexing
+        // tile values, indexed as follows:
+        /* 0  1  2  3  4 
+           5  6  7  8  9
+           10 11 12 13 14
+           15 16 17 18 19
+           20 21 22 23 24 */
         std::array<char, N_TILES> tiles;
+
         char cur_blank_idx = -1;
         char prev_blank_idx = -1;       
 
@@ -29,7 +34,7 @@ namespace Tiles {
     };
 
     // direction that the blank tile moves to get new blank tile position
-    enum class DIRECTION { UP, DOWN, LEFT, RIGHT, COUNT };
+    enum DIRECTION { UP, DOWN, LEFT, RIGHT, COUNT };
 
     // free functions to manipulate the board
     
@@ -38,8 +43,8 @@ namespace Tiles {
     char getBlankIdx(const Board& board);
 
     // get possible new blank indexes
-    std::array<char, static_cast<int>(DIRECTION::COUNT)>
-                                       getNewBlankIdxs(const Board& board);
+    std::array<char, DIRECTION::COUNT>
+    getNewBlankIdxs(const Board& board);
 
     // swap blank tile with new blank tile to get new board configuration
     Board getNewBoard(const Board& board, char newBlankIdx);
